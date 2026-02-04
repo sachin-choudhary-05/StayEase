abeb0357b9f5a39d42d5e2b73ee79c36018b0845
sachin choudhary <sachin68695@gmail.com>
2026-02-04 14:30:03 +0530

    Fix case-sensitive import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Hero from "./componant/landing/hero/hero";
+import Hero from "./componant/landing/hero/Hero";
 import About from "./componant/landing/about/About";
 import Booking from "./componant/landing/booking/Booking";
-import Rooms from "./componant/landing/rooms/rooms";
+import Rooms from "./componant/landing/rooms/Rooms";
 import Newsletter from "./componant/landing/footer/Footer";
 import Navbar from "./componant/navbar/Navbar";
 import Activities from "./componant/landing/activities/Activities";
